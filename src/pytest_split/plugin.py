c614c61722645162191801a504fb46994a190a43
Amin Sadeghi <amin.sadeghi@live.com>
2021-04-07 18:54:09 -0400

    Fixed an indexing bug since Python indexing is [inclusive - exclusive)

--- src/pytest_split/plugin.py
+++ src/pytest_split/plugin.py
@@ -138,7 +138,7 @@ def _calculate_suite_start_and_end_idx(splits, group, items, stored_durations):
     if _is_ipy_notebook(item_node_ids[start_idx]):
         start_idx = _get_boundary_idx_for_ipynb(item_node_ids, start_idx, "start")
     if _is_ipy_notebook(item_node_ids[end_idx - 1]):  # since Python indexing is [a, b)
-        end_idx = _get_boundary_idx_for_ipynb(item_node_ids, end_idx, "end")
+        end_idx = _get_boundary_idx_for_ipynb(item_node_ids, end_idx - 1, "end")
 
     return start_idx, end_idx
 
@@ -155,11 +155,9 @@ def _get_boundary_idx_for_ipynb(item_node_ids, idx, mode):
         raise Exception(f"Unsupported mode: {mode}")
     ipynb_node_id = item_node_ids[idx]
     fname = ipynb_node_id.split("::")[0]
-    # If node_id provided instead of idx
-    # idx = item_node_ids.index(ipynb_node_id)
     same_notebook = True
     while same_notebook:
-        if idx in [0, len(item_node_ids)]:
+        if idx in [0, len(item_node_ids) - 1]:
             break
         idx_next = idx - 1 if mode == "start" else idx + 1
         fname_next = item_node_ids[idx_next].split("::")[0]
